c581d45fe9136df3fb6c7511c5c106f7faf6a6ca
Jasmine <liaolinsb@gmail.com>
2024-08-13 23:35:13 +0800

    0813

--- lab4/flik/TestFilk.java
+++ lab4/flik/TestFilk.java
@@ -1,2 +1,18 @@
-package flik;public class TestFilk {
+package flik;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class TestFilk {
+    @Test
+    public void test127(){
+        assertTrue("127==127",Flik.isSameNumber(127,127));
+    }
+
+    @Test
+    public void test128(){
+        assertTrue("128==128",Flik.isSameNumber(128,128));
+
+    }
+
+
 }
